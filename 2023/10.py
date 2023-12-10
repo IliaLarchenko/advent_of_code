b1fab63e0b4cf777e6886674b271e75169e171fa
Ilia <ilia.larchenko@gmail.com>
2023-12-10 16:18:24 +0700

    Day 10 alternative

--- 2023/10.py
+++ 2023/10.py
@@ -1,7 +1,7 @@
 def prepare_data(file="input.txt"):
     with open(file) as f:
         lines = f.readlines()
-    lines = [x.strip() for x in lines]
+    lines = [list(x.strip()) for x in lines]
 
     return {"lines": lines}
 
@@ -114,6 +114,151 @@ def solve2(lines, **kwargs):
             map_with_gaps[node.i * 2 - 1][node.j * 2] = "L"
         if node.bottom in dist:
             map_with_gaps[node.i * 2 + 1][node.j * 2] = "L"
+
+    # mark the walls
+    next_iter = []
+    for i in range(len(map_with_gaps)):
+        for j in range(len(map_with_gaps[0])):
+            if (
+                i == 0
+                or j == 0
+                or i == len(map_with_gaps) - 1
+                or j == len(map_with_gaps[0]) - 1
+            ):
+                if map_with_gaps[i][j] != "L":
+                    map_with_gaps[i][j] = "O"
+                    next_iter.append((i, j))
+
+    # get the inner nodes
+    while len(next_iter) > 0:
+        i, j = next_iter.pop()
+        map_with_gaps[i][j] = "O"
+        for i1, j1 in [(i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1)]:
+            if (
+                0 <= i1 < len(map_with_gaps)
+                and 0 <= j1 < len(map_with_gaps[0])
+                and map_with_gaps[i1][j1] == "I"
+            ):
+                next_iter.append((i1, j1))
+
+    # count the inner nodes in the original map
+    ans = 0
+    for i in range(len(map_with_gaps)):
+        for j in range(len(map_with_gaps[0])):
+            if map_with_gaps[i][j] == "I" and i % 2 == 0 and j % 2 == 0:
+                ans += 1
+
+    return ans
+
+
+# No graph solution
+def fix_s_tile(lines):
+    # No checks for edge cases
+    # In practice you could just do it mannualy in the input file for speed
+    for i in range(len(lines)):
+        for j in range(len(lines[0])):
+            if lines[i][j] == "S":
+                S = (i, j)
+                break
+
+    i, j = S
+    connections = [False, False, False, False]  # left, right, top, bottom
+    if lines[i + 1][j] in "|LJ":
+        connections[3] = True
+    if lines[i - 1][j] in "|F7":
+        connections[2] = True
+    if lines[i][j + 1] in "-J7":
+        connections[1] = True
+    if lines[i][j - 1] in "-LF":
+        connections[0] = True
+
+    if connections[0] and connections[1]:
+        lines[i][j] = "-"
+    elif connections[0] and connections[2]:
+        lines[i][j] = "J"
+    elif connections[0] and connections[3]:
+        lines[i][j] = "7"
+    elif connections[1] and connections[2]:
+        lines[i][j] = "L"
+    elif connections[1] and connections[3]:
+        lines[i][j] = "F"
+    elif connections[2] and connections[3]:
+        lines[i][j] = "|"
+
+    return S
+
+
+def find_cycle(lines):
+    # No checks for edge cases
+
+    S = fix_s_tile(lines)
+
+    cycle = set()
+    to_visit = set()
+    to_visit.add(S)
+
+    while len(to_visit) > 0:
+        i, j = to_visit.pop()
+        cycle.add((i, j))
+
+        if lines[i][j] == "|":
+            to_add = [(i + 1, j), (i - 1, j)]
+        elif lines[i][j] == "-":
+            to_add = [(i, j + 1), (i, j - 1)]
+        elif lines[i][j] == "L":
+            to_add = [(i - 1, j), (i, j + 1)]
+        elif lines[i][j] == "J":
+            to_add = [(i - 1, j), (i, j - 1)]
+        elif lines[i][j] == "F":
+            to_add = [(i + 1, j), (i, j + 1)]
+        elif lines[i][j] == "7":
+            to_add = [(i + 1, j), (i, j - 1)]
+
+        for i, j in to_add:
+            if (i, j) not in cycle and (i, j) not in to_visit:
+                to_visit.add((i, j))
+
+    # fix the input back
+    lines[S[0]][S[1]] = "S"
+
+    return S, cycle
+
+
+def solve1_no_graph(lines, **kwargs):
+    _, cycle = find_cycle(lines)
+    return len(cycle) // 2
+
+
+def solve2_no_graph(lines, **kwargs):
+    S, cycle = find_cycle(lines)
+
+    # create a map with gaps
+    map_with_gaps = [
+        ["I"] * (len(lines[0]) * 2 - 1) for _ in range((len(lines) * 2 - 1))
+    ]
+
+    # mark the cycle 
+    # everyhting esle is the same as before
+    for i, j in cycle:
+        map_with_gaps[i * 2][j * 2] = "L"
+        if lines[i][j] == "|":
+            map_with_gaps[i * 2 + 1][j * 2] = "L"
+            map_with_gaps[i * 2 - 1][j * 2] = "L"
+        elif lines[i][j] == "-":
+            map_with_gaps[i * 2][j * 2 + 1] = "L"
+            map_with_gaps[i * 2][j * 2 - 1] = "L"
+        elif lines[i][j] == "L":
+            map_with_gaps[i * 2][j * 2 + 1] = "L"
+            map_with_gaps[i * 2 - 1][j * 2] = "L"
+        elif lines[i][j] == "J":
+            map_with_gaps[i * 2][j * 2 - 1] = "L"
+            map_with_gaps[i * 2 - 1][j * 2] = "L"
+        elif lines[i][j] == "7":
+            map_with_gaps[i * 2][j * 2 - 1] = "L"
+            map_with_gaps[i * 2 + 1][j * 2] = "L"
+        elif lines[i][j] == "F":
+            map_with_gaps[i * 2][j * 2 + 1] = "L"
+            map_with_gaps[i * 2 + 1][j * 2] = "L"
 
     # mark the walls
     next_iter = []
