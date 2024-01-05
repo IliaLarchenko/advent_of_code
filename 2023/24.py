49729498681628c78edc11aee89a7c3672920c7f
Ilia <ilia.larchenko@gmail.com>
2024-01-05 19:47:35 +0700

    Day 24 no scipy

--- 2023/24.py
+++ 2023/24.py
@@ -77,3 +77,28 @@ def solve2(lines, **kwargs):
     res = minimize(min_loss, x0, method="Powell")
 
     return round(res["x"][:3].sum())
+
+
+def solve2_alt(lines, **kwargs):
+    lines_coord = [line_to_coord(x) for x in lines]
+    coords = np.stack([np.array(x[0]) for x in lines_coord])
+    speeds = np.stack([np.array(x[1]) for x in lines_coord])
+
+    # Initial guess
+    c = np.random.rand(3)
+    s = np.random.rand(3)
+    ts = np.random.rand(len(lines_coord), 1)
+
+    # Iterative optimization
+    for i in range(20000):
+        # analytical solution for the local optimums
+        # each of the following lines returns the optimal value of the corresponding variable
+        # given the values of the other variables
+        # it works even faster than scipy.optimize for this porblem
+
+        ts = -((coords - c) * (speeds - s)).sum(1) / ((speeds - s) ** 2).sum(1)
+        ts = ts.reshape(len(ts), 1)
+        c = (coords + (speeds - s) * ts.reshape(len(ts), 1)).mean(0)
+        s = (((coords - c) + speeds * ts) * ts).mean(0) / (ts**2).mean()
+
+    return int(c.sum().round())
